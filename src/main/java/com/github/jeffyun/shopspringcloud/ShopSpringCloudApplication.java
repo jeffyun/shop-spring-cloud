a23894bb09677199c2adfd1529956883dcbed46e
jianfeng.wu@ikang.com <123456>
2019-05-25 13:07:17 +0800

    add init

---
+++ src/main/java/com/github/jeffyun/shopspringcloud/ShopSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.github.jeffyun.shopspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopSpringCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jeffyun/shopspringcloud/ShopSpringCloudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.jeffyun.shopspringcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopSpringCloudApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
